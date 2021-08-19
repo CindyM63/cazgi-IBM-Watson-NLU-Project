230a645997ef98bbb9e4417a685d7f32d39ae98f
CindyM63 <77008562+CindyM63@users.noreply.github.com>
2021-08-18 21:49:16 -0400

    Update EmotionTable.js

--- sentimentAnalyzeClient/src/EmotionTable.js
+++ sentimentAnalyzeClient/src/EmotionTable.js
@@ -17,7 +17,7 @@ class EmotionTable extends React.Component {
         <td>{mapentry[1]}</td>
         </tr>
     )
-    })
+    }
                 //Write code to use the .map method that you worked on in the Hands-on React lab to extract the emotions
             }
             </tbody>
